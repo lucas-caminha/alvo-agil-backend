3ee959a226ecf850de91075eef7df1c5fdfea5c7
Lucas Caminha <38120031+lucas-caminha@users.noreply.github.com>
2022-12-18 11:09:32 -0300

    update CORS

--- src/main/java/br/com/ucsal/meta/agil/RestServiceCorsApplication.java
+++ src/main/java/br/com/ucsal/meta/agil/RestServiceCorsApplication.java
@@ -22,7 +22,7 @@ public class RestServiceCorsApplication implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000", "https://main--alvo-agil.netlify.app/")
+		.allowedOrigins("http://localhost:3000", "https://main--alvo-agil.netlify.app/", "https://alvo-agil.netlify.app/")
 		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	} 
 
